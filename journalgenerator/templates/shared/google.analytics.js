048c2b0335ec4c78fe0b7905ab7f2de2bca65754
Eugene Petrenko <eugene.petrenko@gmail.com>
2010-08-03 20:31:25 +0000

    add google analytics both for math and for spbu sites
    git-svn-id: https://secure.diverse.org.ru/jonny/svn/journal/trunk@134 8ccf259f-8e7f-7344-a077-26059c802dd9

---
+++ journalgenerator/templates/shared/google.analytics.js
@@ -0,0 +1,27 @@
+
+if (document.location.host.indexOf('neva.ru') >= 0) {
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-17795545-2']);
+  _gaq.push(['_setDomainName', '.neva.ru']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+}
+
+if (document.location.host.indexOf('spbu.ru') >= 0) {
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-17795545-2']);
+  _gaq.push(['_setDomainName', '.neva.ru']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+}
+
