7198fee086acff0c70312374aa1ef2d83aa39d8e
Eugene Petrenko <eugene.petrenko@gmail.com>
2010-08-31 20:43:29 +0000

    fix google analytics
    git-svn-id: https://secure.diverse.org.ru/jonny/svn/journal/trunk@145 8ccf259f-8e7f-7344-a077-26059c802dd9

--- journalgenerator/templates/shared/google.analytics.js
+++ journalgenerator/templates/shared/google.analytics.js
@@ -16,7 +16,7 @@ if (document.location.host.indexOf('neva.ru') >= 0) {
 
 if (document.location.host.indexOf('spbu.ru') >= 0) {
   var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-17795545-2']);
+  _gaq.push(['_setAccount', 'UA-17795545-1']);
   _gaq.push(['_setDomainName', '.neva.ru']);
   _gaq.push(['_trackPageview']);
 
