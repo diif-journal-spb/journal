3285e240105232123582e98e6476399000eee695
Eugene Petrenko <eugene.petrenko@gmail.com>
2013-03-12 17:12:01 +0000

    remove neva.ru from all possible places
    git-svn-id: https://secure.diverse.org.ru/jonny/svn/journal/trunk@270 8ccf259f-8e7f-7344-a077-26059c802dd9

--- journalgenerator/templates/shared/google.analytics.js
+++ journalgenerator/templates/shared/google.analytics.js
@@ -1,20 +1,5 @@
 
-if (document.location.host.indexOf('neva.ru') >= 0) {
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-17795545-2']);
-  _gaq.push(['_setDomainName', '.neva.ru']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-  setTimeout(function() { window.location="http://www.math.spbu.ru/diffjournal/j"; }, 300);
-}
-
-if (document.location.host.indexOf('spbu.ru') >= 0) {
+(function(){
   var _gaq = _gaq || [];
   _gaq.push(['_setAccount', 'UA-17795545-1']);
   _gaq.push(['_setDomainName', '.math.spbu.ru']);
@@ -25,5 +10,5 @@ if (document.location.host.indexOf('spbu.ru') >= 0) {
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   })();
-}
+})();
 
