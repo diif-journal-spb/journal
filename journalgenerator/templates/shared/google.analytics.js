cd94c31384f804505a05e2ad12f375a8b656f89e
Eugene Petrenko <eugene.petrenko@gmail.com>
2013-11-01 20:45:54 +0000

    new analytics code
    git-svn-id: https://secure.diverse.org.ru/jonny/svn/journal/trunk@303 8ccf259f-8e7f-7344-a077-26059c802dd9

--- journalgenerator/templates/shared/google.analytics.js
+++ journalgenerator/templates/shared/google.analytics.js
@@ -1,8 +1,6 @@
 
-(function(){
   var _gaq = _gaq || [];
   _gaq.push(['_setAccount', 'UA-17795545-1']);
-  _gaq.push(['_setDomainName', '.math.spbu.ru']);
   _gaq.push(['_trackPageview']);
 
   (function() {
@@ -10,5 +8,4 @@
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   })();
-})();
 
