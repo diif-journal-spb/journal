c7ecea64c02cddeaa69a9157591480a76bb05f1f
Eugene Petrenko <eugene.petrenko@gmail.com>
2014-02-08 23:07:10 +0000

    minor fixes, show emails as links, used jquery
    git-svn-id: https://secure.diverse.org.ru/jonny/svn/journal/trunk@320 8ccf259f-8e7f-7344-a077-26059c802dd9

--- journalgenerator/templates/shared/journal.js
+++ journalgenerator/templates/shared/journal.js
@@ -1,2 +1,9 @@
 /* journal specific js goes here */
 
+$(function($){
+ $('div.board .editor span.email').replaceWith(function() {
+    var url = $.trim($(this).text());
+    return '<a href="mailto:' + url + '">' + url + '</a>';
+ });
+});
+
