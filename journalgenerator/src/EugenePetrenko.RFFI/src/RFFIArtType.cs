7d275f8dd3e4fccf18c9104fe19cd0bf508df80e
Eugene Petrenko <eugene.petrenko@gmail.com>
2017-01-21 21:09:20 +0100

    include article types

--- journalgenerator/src/EugenePetrenko.RFFI/src/RFFIArtType.cs
+++ journalgenerator/src/EugenePetrenko.RFFI/src/RFFIArtType.cs
@@ -4,22 +4,23 @@ namespace EugenePetrenko.RFFI
   {
     public readonly string Type;
 
-    private RFFIArtType(string type)
+    private RFFIArtType(string type, string description)
     {
       Type = type;
     }
 
-    public static RFFIArtType RAR = new RFFIArtType("RAR");
-    public static RFFIArtType EDI = new RFFIArtType("EDI");
-    public static RFFIArtType BRV = new RFFIArtType("BRV");
-    public static RFFIArtType CNF = new RFFIArtType("CNF");
-    public static RFFIArtType SCO = new RFFIArtType("SCO");
-    public static RFFIArtType REV = new RFFIArtType("REV");
-    public static RFFIArtType ABS = new RFFIArtType("ABS");
-    public static RFFIArtType REP = new RFFIArtType("REP");
-    public static RFFIArtType COR = new RFFIArtType("COR");
-    public static RFFIArtType PER = new RFFIArtType("PER");
-    public static RFFIArtType MIS = new RFFIArtType("MIS");
-    public static RFFIArtType UNK = new RFFIArtType("UNK");
+    public static RFFIArtType RAR = new RFFIArtType("RAR", "научная статья");
+    public static RFFIArtType EDI = new RFFIArtType("EDI", "редакторская заметка");
+    public static RFFIArtType BRV = new RFFIArtType("BRV", "рецензия");
+    public static RFFIArtType CNF = new RFFIArtType("CNF", "материалы конференции");
+    public static RFFIArtType THS = new RFFIArtType("THS", "тезисы доклада на конференции");
+    public static RFFIArtType SCO = new RFFIArtType("SCO", "краткое сообщение");
+    public static RFFIArtType REV = new RFFIArtType("REV", "обзорная статья");
+    public static RFFIArtType ABS = new RFFIArtType("ABS", "аннотация");
+    public static RFFIArtType REP = new RFFIArtType("REP", "научный отчет");
+    public static RFFIArtType COR = new RFFIArtType("COR", "переписка");
+    public static RFFIArtType PER = new RFFIArtType("PER", "персоналии");
+    public static RFFIArtType MIS = new RFFIArtType("MIS", "разное");
+    public static RFFIArtType UNK = new RFFIArtType("UNK", "неопределен");
   }
 }
